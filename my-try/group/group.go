7d93e0d42cd03eb7af3cf2a882c8960c7233f675
ADMIN <admin@MacBook-Pro-4.local>
2026-05-09 12:00:19 +0530

    complete group therapy exercise

--- my-try/group/group.go
+++ my-try/group/group.go
@@ -1,3 +1,4 @@
 package group
 
 // Your Group type goes here!
+type Group[T any] []T
--- my-try/print/print_test.go
+++ my-try/print/print_test.go
@@ -7,11 +7,33 @@ import (
 	"github.com/bitfield/print"
 )
 
-func TestPrintAnythingTo_PrintsToGivenWriter(t *testing.T) {
+func TestPrintAnythingTo_PrintsStringToGivenWriter(t *testing.T) {
 	t.Parallel()
 	buf := new(bytes.Buffer)
 	print.PrintAnythingTo(buf, "Hello, world")
 	want := "Hello, world\n"
+	got := buf.String()
+	if want != got {
+		t.Errorf("want %q, got %q", want, got)
+	}
+}
+
+func TestPrintAnythingTo_PrintsIntToGivenWriter(t *testing.T) {
+	t.Parallel()
+	buf := new(bytes.Buffer)
+	print.PrintAnythingTo(buf, 123)
+	want := "123\n"
+	got := buf.String()
+	if want != got {
+		t.Errorf("want %q, got %q", want, got)
+	}
+}
+
+func TestPrintAnythingTo_PrintsFloatToGivenWriter(t *testing.T) {
+	t.Parallel()
+	buf := new(bytes.Buffer)
+	print.PrintAnythingTo(buf, 123.22)
+	want := "123.22\n"
 	got := buf.String()
 	if want != got {
 		t.Errorf("want %q, got %q", want, got)
