f6d739e011727de80ba3f176c629491ead76af57
ADMIN <admin@MacBook-Pro-4.local>
2026-05-09 14:59:43 +0530

    Solve intish exercise and apply type approximations

--- my-try/intish/intish.go
+++ my-try/intish/intish.go
@@ -2,6 +2,10 @@ package intish
 
 // Your Intish interface goes here!
 
+type Intish interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
+}
+
 func IsPositive[T Intish](v T) bool {
 	return v > 0
 }
